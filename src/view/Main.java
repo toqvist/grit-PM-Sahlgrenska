6e052b30bd6ef64116bd63a9d7848b48dc5c9476
toqvist <tobias-ahlqvist@outlook.com>
2022-02-10 15:59:54 +0100

    Implemented login interface

--- src/view/Main.java
+++ src/view/Main.java
@@ -1,7 +1,67 @@
 package view;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        boolean interfaceLooping = false;
+        
+        //Prompt user for login details, if successfully authenticated send user to menu interface.
+        if (login()) {
+            interfaceLooping = true;
+        }
+    
+        //Menu interface
+        while(interfaceLooping) {
+
+            System.out.println("Welcome to Sahlgrenska Sjukhus!");
+            System.out.println("[1] - Exit Application");
+            
+            Scanner scanner = new Scanner(System.in);
+            int userInput=0;
+
+            try {
+                    
+                userInput = scanner.nextInt();
+            } catch (Exception e) {
+                System.out.println("Input must be a valid integer from printed choices!");
+            } 
+
+            switch(userInput) {
+                    
+                case 0: //Do nothing
+                    break;
+                case 1:
+                    interfaceLooping = false;
+                    break;
+            }
+
+            //Wait for user input, so that the console does not become crowded with menu choices.
+            System.out.println("Press any key to continue");
+            System.in.read();
+        }
+    }
+
+    public static boolean login () {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter login details");
+        
+        System.out.println("Enter Username: ");
+        String username = scanner.next();
+
+        System.out.println("Enter password: ");
+        String password = scanner.next();
+
+
+        return true;
+        // if (authenticate(username, password)) {
+        //     return true;
+        // } else {
+        //     return false;
+        // }
+        
     }
 }
