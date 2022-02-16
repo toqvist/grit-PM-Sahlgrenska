f7b191c70bee5b1737e3e0882ead8a89bbbde66a
toqvist <tobias-ahlqvist@outlook.com>
2022-02-16 09:39:46 +0100

    Added class 'equipment'

---
+++ src/model/Equipment.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Equipment {
+    String name;
+    String description;
+    int amount;
+
+    public Equipment (String name, String description, int amount) {
+        this.name = name;
+        this.description = description;
+        this.amount = amount;
+    }
+}
